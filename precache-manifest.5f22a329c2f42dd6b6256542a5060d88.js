8816c197277d04ec4d820010a95b2f413ac79b00
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-09-18 14:23:48 +0300

    Updates

---
+++ precache-manifest.5f22a329c2f42dd6b6256542a5060d88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05274fa752f7d4a680902caae630650",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "0060f0d09306859f0998",
+    "url": "/react_add-todo-form/static/css/main.f28dbc3e.chunk.css"
+  },
+  {
+    "revision": "a6e4de08155c831d318c",
+    "url": "/react_add-todo-form/static/js/2.b2a64308.chunk.js"
+  },
+  {
+    "revision": "0060f0d09306859f0998",
+    "url": "/react_add-todo-form/static/js/main.95b41dda.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  },
+  {
+    "revision": "8256ad05df049a512c23e70a9268386d",
+    "url": "/react_add-todo-form/static/media/Drawing-Pin.8256ad05.png"
+  },
+  {
+    "revision": "88c1fd2facedc0892968606fdf5a9c36",
+    "url": "/react_add-todo-form/static/media/background.88c1fd2f.jpg"
+  },
+  {
+    "revision": "9b81c298531cb0246720056713a02a9a",
+    "url": "/react_add-todo-form/static/media/mail.9b81c298.png"
+  },
+  {
+    "revision": "7bd46cff95f27a4e6f6fc60da31ceca3",
+    "url": "/react_add-todo-form/static/media/user.7bd46cff.png"
+  },
+  {
+    "revision": "c6569e20ade5a81d7e82ce39259a7ed4",
+    "url": "/react_add-todo-form/static/media/worldwide.c6569e20.png"
+  }
+]);
